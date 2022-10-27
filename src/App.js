d3117721045d7c75a258f2d197f7259e01f6caec
Yudhitia Rizky <yudhitiarizky2002@gmail.com>
2022-10-27 22:51:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Listdata = ({title}) => {
+	return (
+	  <div className='list-data'>
+		{title}
+	  </div>
+	);
+};
+
+const App = () => {
+	const[todos, setTodos] = useState([]);
+	const[newTodo, setNewTodo] = useState('');	
+	
+	const handleChange = (event) => {
+		setNewTodo(event.target.value);
+	};
+	
+	const addNewTodo = () => {
+		setTodos([...todos, { title: newTodo, id: Date.now()}]);
+	};
+	return (
+
+		<div className='container'>
+      <div className='form'>
+        <input value={newTodo} onChange={handleChange} />
+        <button onClick={addNewTodo}>Add</button>
+      </div>
+    
+			<h1>Todo List</h1>
+
+			<div className="list-container">
+				{todos.map((todo) => {
+					return (
+						<Listdata
+							title={todo.title} 
+							key={`todo-${todo.id}`}/>
+					);
+				})}
+			</div>
+			
+		</div>
+    );
+};
 
 export default App;
